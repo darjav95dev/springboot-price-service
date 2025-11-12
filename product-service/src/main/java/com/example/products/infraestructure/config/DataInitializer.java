b60b35215855fa17c9429859396973a8c6ab76ef
Dario Javier Diaz Caballero <dariojavierdiazcaballero@Mac-mini-de-Dario.local>
2025-11-12 12:31:07 +0000

    Delete import unused

--- product-service/src/main/java/com/example/products/infraestructure/config/DataInitializer.java
+++ product-service/src/main/java/com/example/products/infraestructure/config/DataInitializer.java
@@ -5,7 +5,6 @@ import com.example.products.infraestructure.persistence.ProductJpaRepository;
 import com.example.products.infraestructure.persistence.entity.ProductEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
